496d0c2be16ecc94e12f080151feb4ea4ab8089a
Shorya Jain <shoryajainstedwards@gmail.com>
2023-06-17 23:08:35 +0530

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/", "/sign-in", "/user/:path*"]
+    publicRoutes: ["/", "/sign-in", "/user/:path*", "/api/webhooks/user"]
 })
 
 export const config = {
