3d23ab728732c0492c2414b97a578e80c211245e
Shorya Jain <shoryajainstedwards@gmail.com>
2023-07-02 19:59:08 +0530

    designed sign up page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs"
 
 const page = () => {
     return (
-        <div>
+        <div className="w-screen min-h-screen bg-dark-body flex justify-center items-center overflow-hidden">
             <SignIn />
         </div>
     )
--- src/components/UserPostCard.tsx
+++ src/components/UserPostCard.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 const UserPostCard = (props: Props) => {
     return (
-        <div className="rounded-xl w-[260px] m-4 bg-dark-highlights text-white
+        <div className="rounded w-[260px] m-4 bg-dark-highlights text-white
         hover:-translate-y-4 duration-500 hover:shadow-xl hover:shadow-gray-700">
             <div className="relative h-[180px] w-full">
-                <Image src={props.imageURL} alt="recipe-image" fill={true} className="object-cover rounded-xl" />
+                <Image src={props.imageURL} alt="recipe-image" fill={true} className="object-cover rounded" />
             </div>
-            <div className="flex flex-col my-2 pt-1 pb-2 px-3 cursor-pointer">
+            <div className="flex flex-col mt-2 mb-5 pt-1 pb-2 px-3 cursor-pointer">
                 <h5 className="text-xl font-semibold mb-2">{props.title}</h5>
                 <p className="rounded-full w-max text-xs py-1 px-2 mb-5 border border-white">{props.categoryName}</p>
             </div>
