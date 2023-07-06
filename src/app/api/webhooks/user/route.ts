77ca92429e28cdf57db21dd5bb1b684253fc343d
Shorya Jain <shoryajainstedwards@gmail.com>
2023-07-06 16:05:14 +0530

    testing clerk webhook changes

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -26,14 +26,15 @@ async function handler(req: Request) {
     }
 
     const eventType: EventType = evt.type
-    const { id, first_name, last_name } = evt.data
+    const { id, first_name, last_name, imageUrl } = evt.data
 
     if (eventType === "user.created") {
         await prisma.user.create({
             data: {
                 id: id as string,
                 first_name: first_name as string,
-                last_name: last_name as string
+                last_name: last_name as string,
+                profileImageUrl: imageUrl as string
             }
         })
     }
